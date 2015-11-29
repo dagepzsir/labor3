b732287c0f9b39080af60490132093dd624b65d4
dagepzsir <braunadam@hotmail.com>
2015-11-29 23:20:46 +0100

    Initial Commit

---
+++ labor3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  labor3
+//
+//  Created by Braun Ádám on 2015. 11. 29..
+//  Copyright © 2015. asd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
