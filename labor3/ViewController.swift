aace6e81883849b39a9ce8d4215112f2e15fedb6
dagepzsir <braunadam@hotmail.com>
2015-12-12 13:51:12 +0100

    base upload

--- labor3/ViewController.swift
+++ labor3/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum OperationType {
+        case Add
+        case Mul
+        case Div
+    }
+    
+    //Connectors
+    @IBOutlet weak var inputTextFieldA: UITextField!
+    @IBOutlet weak var inputTextFieldB: UITextField!
+    @IBOutlet weak var segmentedController: UISegmentedControl!
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    //Variables
+    var operationType = OperationType.Add
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +36,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Events
+    @IBAction func calculateButtonTouchUpInside(sender: AnyObject) {
+        let numberFormater = NSNumberFormatter()
+        var result: Double
+        var operationChar: String
+        if let textA = inputTextFieldA.text, textB = inputTextFieldB.text, a = numberFormater.numberFromString(textA)?.doubleValue, b = numberFormater.numberFromString(textB)?.doubleValue {
+            switch operationType
+            {
+            case .Add:
+                 result = a + b
+                 operationChar = "+"
+            case .Div:
+                 result = a / b
+                 operationChar = "/"
+            case .Mul:
+                 result = a * b
+                 operationChar = "*"
+            }
+            
+            resultLabel.text = "\(result)"
+            historyTextView.text = "\(a) \(operationChar) \(b) = \(result)" + historyTextView.text
+            
+        }
+    }
+    @IBAction func backgroundTouchUpInside(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
-
+    @IBAction func operationSelectorValueChanged(sender: AnyObject) {
+        switch segmentedController.selectedSegmentIndex
+        {
+        case 0:
+            operationType = .Add
+        case 1:
+            operationType = .Mul
+        case 2:
+            operationType = .Div
+        default:
+            operationType = .Add
+        }
+    }
 }
-
